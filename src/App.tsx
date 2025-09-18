5901f80dee421ffcf150ee913ae7759f894419cf
Bod <164583370+Bodzaman@users.noreply.github.com>
2025-09-18 15:00:11 +0100

    feat: Add main App component that loads POSDesktop

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import POSDesktop from './pages/POSDesktop'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <POSDesktop />
+    </div>
+  )
+}
+
+export default App
