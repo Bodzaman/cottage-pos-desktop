b1bc3ff612f80250fb2017e30f081218143fd6c0
Bod <164583370+Bodzaman@users.noreply.github.com>
2026-01-08 01:58:49 +0000

    chore: sync POS Desktop UI changes (patch)

---
+++ renderer/components/CustomerComponents.tsx
@@ -0,0 +1,20 @@
+/**
+ * Ruby Red Customer Components Export
+ * Centralized export for all customer-themed components
+ */
+
+export { default as CustomerCard } from './CustomerCard';
+export { default as CustomerButton } from './CustomerButton';
+export { CustomerSidebar, CustomerNavItem } from './CustomerSidebar';
+export { default as CustomerInput } from './CustomerInput';
+export { default as CustomerBadge } from './CustomerBadge';
+export { default as CustomerHeader } from './CustomerHeader';
+
+// Export design system
+export {
+  RubyRedColors,
+  rubyStyles,
+  rubyEffects,
+  RubyRedCustomerStyles,
+  RubyRedCustomerDesignSystem
+} from '../utils/RubyRedCustomerDesign';
