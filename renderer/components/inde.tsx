27e9bf9ee9cfa607ed092ff0fde11bc11a5a78ed
Bod <164583370+Bodzaman@users.noreply.github.com>
2026-01-08 02:06:04 +0000

    chore: sync POS Desktop UI changes (patch)

---
+++ renderer/components/inde.tsx
@@ -0,0 +1,14 @@
+
+// Export all components from this file for easy importing
+export { default as CustomerDetailsModal } from './CustomerDetailsModal';
+export { default as CustomerSummaryBadge } from './CustomerSummaryBadge';
+export { default as GooglePlacesAutocomplete } from './GooglePlacesAutocomplete';
+export { default as RouteVisualization } from './RouteVisualization';
+export { default as EnhancedDeliveryAddressForm } from './EnhancedDeliveryAddressForm';
+
+// Add other component exports here as needed
+export * from './CustomerDetailsModal';
+export * from './CustomerSummaryBadge';
+export * from './GooglePlacesAutocomplete';
+export * from './RouteVisualization';
+export * from './EnhancedDeliveryAddressForm';
